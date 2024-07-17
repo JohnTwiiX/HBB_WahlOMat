11fc62fe0511371a6b870da1f725f8ee0c0af99b
TwiiX <john.fieweger98@gmail.com>
2024-07-17 19:07:30 +0200

    first commit

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    "projectId": "hhb-wahlomat",
+    "appId": "1:808140226565:web:6181e7c9661bf6ae4c5c6c",
+    "storageBucket": "hhb-wahlomat.appspot.com",
+    "apiKey": "AIzaSyBo2D8hCG8KhzQK72XkKjw22WEaUrsP_2g",
+    "authDomain": "hhb-wahlomat.firebaseapp.com",
+    "messagingSenderId": "808140226565"
+};
